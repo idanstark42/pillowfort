7773a4ee2dd477e3e2fa6b3a62cc6b46c713b361
Idan Stark <idanstark42@gmail.com>
2022-05-15 17:57:08 +0300

    Added attendence

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -1,27 +1,43 @@
-import { ResponsiveContainer, AreaChart, XAxis, Area } from 'recharts'
-import { Link } from 'react-router-dom'
+import { ResponsiveContainer, BarChart, Bar, AreaChart, XAxis, YAxis, Area } from 'recharts'
 
 export default function Stats ({ workoutlog }) {
-  const graphData = workoutlog.data.log
-    .map(entry => entry.exercises.reduce((datum, exercise) => {
-      return Object.assign(datum, { [exercise.name]: exercise.toNumber() })
-    }, { name: entry.date.format('DD/MM/yyyy') }))
+  const attendanceData = workoutlog.entries.map(entry => entry.date).filter(Boolean).reduce((months, date) => {
+    const monthStr = date.substr(date.indexOf('.') + 1)
+    let monthEntry = months.find(month => month.month === monthStr)
+    if (!monthEntry) {
+      monthEntry = { month: monthStr, attendance: 0 }
+      months.push(monthEntry)
+    }
+    monthEntry.attendance = monthEntry.attendance + 1
+    return months
+  }, [])
+  console.log(attendanceData)
+
+  const graphData = workoutlog.entries
+    .map(entry => entry.exerciseEntries.reduce((datum, exEntry) => {
+      return Object.assign(datum, { [exEntry.exercise.name]: exEntry.exercise.toNumber(exEntry.performence) })
+    }, { name: entry.date }))
     .filter(({ name }) => name !== 'Invalid date')
 
   return <div className='stats'>
-    <div className='exercises'>
-      {workoutlog.data.exercises.map(exercise => <div id={exercise.name} key={exercise.name} className='exercise card'>
-        <div className='title'>{exercise.name}</div>
-        <ResponsiveContainer width='100%' height='80%'>
-          <AreaChart data={graphData} margin={{ top: 0, right: 0, left: -30, bottom: 0 }}>
-            <Area dataKey={exercise.name} fill="#191970" stroke="#101035" />
-            <XAxis dataKey='name' stroke="#101035"/>
-          </AreaChart>
-        </ResponsiveContainer>
-      </div>)}
+    <div className='card'>
+      <div className='title'>Attendence/month</div>
+      <ResponsiveContainer width='100%' height='100%'>
+        <BarChart data={attendanceData} margin={{ top: 10, right: 10, left: -40, bottom: 40 }}>
+          <Bar dataKey='attendance' fill="#663300" stroke="#663300" />
+          <XAxis dataKey='month' tickLine={false} stroke="#663300"/>
+          <YAxis tickLine={false} stroke="#663300"/>
+        </BarChart>
+      </ResponsiveContainer>
     </div>
-    <div className='footer'>
-      <Link id="new" to="/new">Start new exercise</Link>
-    </div>
+    {workoutlog.exercises.map(exercise => <div id={exercise.name} key={exercise.name} className='exercise card'>
+      <div className='title'>{exercise.name}</div>
+      <ResponsiveContainer width='100%' height='80%'>
+        <AreaChart data={graphData} margin={{ top: 0, right: 0, left: -30, bottom: 0 }}>
+          <Area dataKey={exercise.name} fill="#663300" stroke="#663300" />
+          <XAxis dataKey='name' stroke="#663300"/>
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>)}
   </div>
 }
