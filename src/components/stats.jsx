ecb22e1752e2d85efcf8f801985ab600802c0f6a
Idan Stark <idanstark42@gmail.com>
2022-06-01 11:38:12 +0300

    Fixed attendence stats

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -8,11 +8,10 @@ export default function Stats () {
   const navigate = useNavigate()
   const [attendanceData, setAddendenceData] = useState()
   useEffect(() => {
-    if (log === REQUIRES_LOGIN)
-      return navigate('/')
+    if (log === REQUIRES_LOGIN) return navigate('/')
 
     setAddendenceData(log.entries.map(entry => entry.date).filter(Boolean).reduce((months, date) => {
-      const monthStr = date.substr(date.indexOf('.') + 1)
+      const monthStr = date.substr(date.indexOf('.') + 1).split('.').map(str => String(Number(str))).join('.')
       let monthEntry = months.find(month => month.month === monthStr)
       if (!monthEntry) {
         monthEntry = { month: monthStr, attendance: 0 }
@@ -32,9 +31,9 @@ export default function Stats () {
     <div className='card'>
       <div className='title'>Attendence/month</div>
       <ResponsiveContainer width='100%' height='100%'>
-        <BarChart data={attendanceData} margin={{ top: 10, right: 10, left: -40, bottom: 40 }}>
+        <BarChart data={attendanceData} margin={{ top: 10, right: 10, left: -40, bottom: 80 }}>
           <Bar dataKey='attendance' fill="#663300" stroke="#663300" />
-          <XAxis dataKey='month' tickLine={false} stroke="#663300"/>
+          <XAxis dataKey='month' angle={-45} textAnchor="end" stroke="#663300"/>
           <YAxis tickLine={false} stroke="#663300"/>
         </BarChart>
       </ResponsiveContainer>
