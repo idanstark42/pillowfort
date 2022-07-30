32eca3dae2e033743ddaaa17c5f93beaca980374
Idan Stark <idanstark42@gmail.com>
2022-07-30 19:38:57 +0300

    Added exercises graphs

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -6,10 +6,15 @@ import { useLog, REQUIRES_LOGIN } from './log-context'
 export default function Stats () {
   const { log } = useLog()
   const navigate = useNavigate()
+  const [exercises, setExercises] = useState()
   const [attendanceData, setAddendenceData] = useState()
   const [successesData, setSuccessesData] = useState()
+  const [exerciseSuccessesData, setExerciseSuccessesData] = useState()
+
   useEffect(() => {
     if (log === REQUIRES_LOGIN) return navigate('/')
+
+    setExercises(log.entries[log.entries.length - 1].exerciseEntries.map(exEntry => exEntry.exercise))
 
     setAddendenceData(log.entries.map(entry => entry.date).filter(Boolean).reduce((months, date) => {
       const monthStr = date.substr(date.indexOf('.') + 1).split('.').map(str => String(Number(str))).join('.')
@@ -25,9 +30,21 @@ export default function Stats () {
     setSuccessesData(log.entries.filter(entry => Boolean(entry.date)).map(entry =>
       ({ date: entry.date, successes: entry.exerciseEntries.filter(exEntry => exEntry.success()).length })))
 
-  }, [log, navigate, setAddendenceData, setSuccessesData])
+    setExerciseSuccessesData(log.entries.filter(entry => Boolean(entry.date)).reduce((months, entry) => {
+      const monthStr = entry.date.substr(entry.date.indexOf('.') + 1).split('.').map(str => String(Number(str))).join('.')
+      let monthEntry = months.find(month => month.month === monthStr)
+      if (!monthEntry) {
+        monthEntry = { month: monthStr, ...entry.exerciseEntries.reduce((all, exEntry) => ({ ...all, [exEntry.exercise.name]: 0 }), { }) }
+        months.push(monthEntry)
+      }
+      entry.exerciseEntries.filter(exEntry => exEntry.success()).forEach(exEntry => { monthEntry[exEntry.exercise.name] = monthEntry[exEntry.exercise.name] + 1 })
+      return months
+    }, []))
+    console.log(exerciseSuccessesData)
 
-  if (log === REQUIRES_LOGIN) {
+  }, [log, navigate, setExercises, setAddendenceData, setSuccessesData, setExerciseSuccessesData])
+
+  if (log === REQUIRES_LOGIN || !exercises) {
     return <div className='loading' />
   }
 
@@ -54,5 +71,17 @@ export default function Stats () {
         </LineChart>
       </ResponsiveContainer>
     </div>
+
+    {exercises.map(exercise => <div className='card'>
+      <div className='title'>{exercise.name.replace('_', ' ')} success rate</div>
+      <ResponsiveContainer width='100%' height='80%'>
+        <LineChart data={exerciseSuccessesData} margin={{ top: 10, right: 10, left: -40, bottom: 80 }}>
+          <Line dataKey={exercise.name} dot={false} fill='#663300' stroke='#663300' />
+          <XAxis dataKey='month' angle={-45} textAnchor='end' stroke='#663300' />
+          <YAxis tickLine={false} stroke='#663300'/>
+        </LineChart>
+      </ResponsiveContainer>
+    </div>)}
+
   </div>
 }
