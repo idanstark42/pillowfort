7b5d71b9090dae326b6b3547f126f7df9f67aec0
Idan Stark <idanstark42@gmail.com>
2023-09-01 17:08:12 +0300

    another attempt to fix the expiration problem

--- src/components/godsheets-context.jsx
+++ src/components/godsheets-context.jsx
@@ -11,7 +11,6 @@ export const useGodSheets = () => useContext(GodSheetsContext)
 
 export function WithGodSheets ({ children }) {
   const [godSheets, setGodSheets] = useState(false)
-  console.log(Cookies.get(CONNECTION_KEY))
   const [years, setYears] = useState(JSON.parse(Cookies.get(CONNECTION_KEY) || '{}'))
 
   const addYear = async url => {
@@ -31,7 +30,7 @@ export function WithGodSheets ({ children }) {
   }
 
   const save = newYears => {
-    Cookies.set(CONNECTION_KEY, JSON.stringify(newYears), { expires: 100 * 365 * 24 * 60 * 60 })
+    Cookies.set(CONNECTION_KEY, JSON.stringify(newYears), { expires: 365 })
     setYears(newYears)
   }
 
