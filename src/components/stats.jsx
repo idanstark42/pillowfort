0ff25078335ff1e019cdbb19fbc1b51a1c983682
Idan Stark <idanstark42@gmail.com>
2023-09-01 18:06:20 +0300

    adding stats

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -1,87 +1,71 @@
-import { useState, useEffect } from 'react'
-import { ResponsiveContainer, LineChart, Line, BarChart, Bar, XAxis, YAxis } from 'recharts'
+import {  useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, Tooltip, AreaChart, Area } from 'recharts'
 import { useNavigate } from 'react-router'
-// import { useLog, REQUIRES_LOGIN } from './log-context'
+import { FaArrowLeft } from 'react-icons/fa'
+import BarLoader from 'react-spinners/BarLoader'
+
+import { useGodSheets } from './godsheets-context'
+
+const EXPENSE_TYPE_COLORS = [
+  { name: 'living', color: '#FF6464' },
+  { name: 'groceries', color: '#FFB764' },
+  { name: 'entretainment', color: '#FFE364' },
+  { name: 'dining', color: '#646464' },
+  { name: 'projects', color: '#6494FF' },
+  { name: 'books', color: '#64FFB9' },
+  { name: 'charity', color: '#64FF64' },
+  { name: 'social', color: '#B9B9B9' },
+  { name: 'transportation', color: '#64FF64' },
+]
+
+const INCOME_TYPE_COLORS = [
+  { name: 'freelance', color: '#FF6464' },
+  { name: 'employee', color: '#FFB764' }
+]
 
 export default function Stats () {
-  // const { log } = useLog()
-  // const navigate = useNavigate()
-  // const [exercises, setExercises] = useState()
-  // const [attendanceData, setAddendenceData] = useState()
-  // const [successesData, setSuccessesData] = useState()
-  // const [exerciseSuccessesData, setExerciseSuccessesData] = useState()
+  const { godSheets } = useGodSheets()
+  const current = godSheets[new Date().getFullYear()]
+  console.log(current)
+  
+  const navigate = useNavigate()
 
-  // useEffect(() => {
-  //   if (log === REQUIRES_LOGIN) return navigate('/')
+  useEffect(() => {
+    if (!current) return navigate('/')
+  }, [current, navigate])
 
-  //   setExercises(log.entries[log.entries.length - 1].exerciseEntries.map(exEntry => exEntry.exercise))
+  if (!godSheets || !current) return <div className='loader'><BarLoader color='#2d3f47' loading={true} size={100} /></div>
 
-  //   setAddendenceData(log.entries.map(entry => entry.date).filter(Boolean).reduce((months, date) => {
-  //     const monthStr = date.substr(date.indexOf('.') + 1).split('.').map(str => String(Number(str))).join('.')
-  //     let monthEntry = months.find(month => month.month === monthStr)
-  //     if (!monthEntry) {
-  //       monthEntry = { month: monthStr, attendance: 0 }
-  //       months.push(monthEntry)
-  //     }
-  //     monthEntry.attendance = monthEntry.attendance + 1
-  //     return months
-  //   }, []))
+  return <div className='stats'>
+    <div className='icon' style={{ position: 'absolute', left: '1rem' }}><Link to='/'><FaArrowLeft style={{ color: 'var(--main)' }} /></Link></div>
+    <div className='title'>statistics</div>
 
-  //   setSuccessesData(log.entries.filter(entry => Boolean(entry.date)).map(entry =>
-  //     ({ date: entry.date, successes: entry.exerciseEntries.filter(exEntry => exEntry.success()).length })))
-
-  //   setExerciseSuccessesData(log.entries.filter(entry => Boolean(entry.date)).reduce((months, entry) => {
-  //     const monthStr = entry.date.substr(entry.date.indexOf('.') + 1).split('.').map(str => String(Number(str))).join('.')
-  //     let monthEntry = months.find(month => month.month === monthStr)
-  //     if (!monthEntry) {
-  //       monthEntry = { month: monthStr, ...entry.exerciseEntries.reduce((all, exEntry) => ({ ...all, [exEntry.exercise.name]: 0 }), { }) }
-  //       months.push(monthEntry)
-  //     }
-  //     entry.exerciseEntries.filter(exEntry => exEntry.success()).forEach(exEntry => { monthEntry[exEntry.exercise.name] = monthEntry[exEntry.exercise.name] + 1 })
-  //     return months
-  //   }, []))
-
-  // }, [log, navigate, setExercises, setAddendenceData, setSuccessesData, setExerciseSuccessesData])
-
-  // if (log === REQUIRES_LOGIN || !exercises) {
-  //   return <div className='loading' />
-  // }
-
-  // return <div className='stats'>
-  // <div className='title'>STATISTICS</div>
-  //   <div className='card'>
-  //     <div className='title'>Attendence/month</div>
-  //     <ResponsiveContainer width='100%' height='100%'>
-  //       <BarChart data={attendanceData} margin={{ top: 10, right: 10, left: -40, bottom: 80 }}>
-  //         <Bar dataKey='attendance' fill='#663300' stroke='#663300' />
-  //         <XAxis dataKey='month' angle={-45} textAnchor='end' stroke='#663300'/>
-  //         <YAxis tickLine={false} stroke='#663300'/>
-  //       </BarChart>
-  //     </ResponsiveContainer>
-  //   </div>
-
-  //   <div className='card'>
-  //     <div className='title'>Successes/workout</div>
-  //     <ResponsiveContainer width='100%' height='100%'>
-  //       <LineChart data={successesData} margin={{ top: 10, right: 10, left: -40, bottom: 80 }}>
-  //         <Line dataKey='successes' dot={false} fill='#663300' stroke='#663300' />
-  //         <XAxis dataKey='date' angle={-45} textAnchor='end' stroke='#663300' tickFormatter={value => value.split('.').splice(1, 2).join()} />
-  //         <YAxis tickLine={false} stroke='#663300'/>
-  //       </LineChart>
-  //     </ResponsiveContainer>
-  //   </div>
-
-  //   {exercises.map(exercise => <div className='card'>
-  //     <div className='title'>{exercise.name.replace('_', ' ')} success rate</div>
-  //     <ResponsiveContainer width='100%' height='80%'>
-  //       <LineChart data={exerciseSuccessesData} margin={{ top: 10, right: 10, left: -40, bottom: 80 }}>
-  //         <Line dataKey={exercise.name} dot={false} fill='#663300' stroke='#663300' />
-  //         <XAxis dataKey='month' angle={-45} textAnchor='end' stroke='#663300' />
-  //         <YAxis tickLine={false} stroke='#663300'/>
-  //       </LineChart>
-  //     </ResponsiveContainer>
-  //   </div>)}
-
-  // </div>
-  return ''
+    <ResponsiveContainer width='90%' height='40%'>
+      <BarChart data={current.expenseTypes}>
+        {EXPENSE_TYPE_COLORS.map(({ name, color }) => <Bar dataKey={name} fill={color} stroke={color} stackId='1' />)}
+        <XAxis dataKey='month' height={1}/>
+        <YAxis tickLine={false} stroke='#2d3f47'/>
+        <Tooltip contentStyle={{ fontFamily: 'Comfortaa', fontSize: 12 }} formatter={value => Math.round(value)}/>
+      </BarChart>
+    </ResponsiveContainer>
+    <ResponsiveContainer width='90%' height='40%'>
+      <AreaChart data={current.projection}>
+        <Area type='monotone' dataKey='liquid' fill="#FF6464" stroke="#FF6464" fillOpacity={0.5} stackId='1' />
+        <Area type='monotone' dataKey='stocks' fill="#6494FF" stroke="#6494FF" fillOpacity={0.5} stackId='1' />
+        <Area type='monotone' dataKey='total' stroke="#000000" fillOpacity={0}  strokeOpacity={0} />
+        <XAxis dataKey='date' height={1}/>
+        <YAxis tickLine={false} stroke='#2d3f47'/>
+        <Tooltip contentStyle={{ fontFamily: 'Comfortaa', fontSize: 12 }} formatter={value => Math.round(value)}/>
+      </AreaChart>
+    </ResponsiveContainer>
+    <ResponsiveContainer width='90%' height='40%'>
+      <BarChart data={current.incomeTypes}>
+        {INCOME_TYPE_COLORS.map(({ name, color }) => <Bar dataKey={name} fill={color} stroke={color} stackId='1' />)}
+        <XAxis dataKey='month' height={1}/>
+        <YAxis tickLine={false} stroke='#2d3f47'/>
+        <Tooltip contentStyle={{ fontFamily: 'Comfortaa', fontSize: 12 }} formatter={value => Math.round(value)}/>
+      </BarChart>
+    </ResponsiveContainer>
+  </div>
 }
